041ed2296c7465b34add57deeec3069a373b5095
abhay040498 <63642084+abhay040498@users.noreply.github.com>
2020-04-14 18:03:47 +0530

    Add files via upload

---
+++ highestmarks.cxx
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int i , j, high ;
+	int marks[5][3];
+	printf(" enter the marks of the students \n");
+	for (i=0;i<5;i++)
+	{
+		printf("enter the marks of the students %d\n",i+1);
+		for (j=0;j<3;j++)
+		scanf("%d",&marks[i][j]);
+	}
+	high=marks[0][0];
+	for(i=0;i<3;i++)
+	{
+		for (j=0;j<5;j++)
+		{
+			if (marks[j][i]>=high)
+			high=marks[j][i];
+		}
+		printf("\n the highest marks in %d subject is marks=%d \n ",i+1, high);
+		high=marks[0][i+1];
+	}
+}
